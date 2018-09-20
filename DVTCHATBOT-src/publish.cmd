04b85b0becee90596074d84f45d7def1be100f50
Bets Tadesse <betsegaw@gmail.com>
2018-09-20 15:37:24 +0200

    Initial commit

---
+++ DVTCHATBOT-src/publish.cmd
@@ -0,0 +1,3 @@
+nuget restore
+msbuild Microsoft.Bot.Sample.QnABot.sln -p:DeployOnBuild=true -p:PublishProfile=dvtchatbot-Web-Deploy.pubxml -p:Password=mystfaHMkhj4g0z9FQA8KE0iRWbXGklket6jWehWT5uHlb98AvTs4nLSr3Y1
+
